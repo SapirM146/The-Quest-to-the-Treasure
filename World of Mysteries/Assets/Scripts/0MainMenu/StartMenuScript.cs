b9d7a302a64f97f25d8a911d3c79388f9b6d6b58
SapirM146 <melker4@gmail.com>
2020-05-26 17:58:45 +0300

    Add Unity project

---
+++ World of Mysteries/Assets/Scripts/0MainMenu/StartMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartMenuScript : MonoBehaviour
+{
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ World of Mysteries/Assets/Scripts/2EndMenu/EndMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndMenuScript : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None; // unlock cursor
+        Cursor.visible = true; // show curser
+    }
+
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene(1); // return to first level
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
