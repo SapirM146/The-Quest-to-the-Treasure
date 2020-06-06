860916e18de5c7fb8a9109d3c78b441871b76101
SapirM146 <melker4@gmail.com>
2020-06-06 20:44:11 +0300

    Add shoting effect, crosshair and more assets

---
+++ World of Mysteries/Assets/Scripts/1Level01/BillBoardScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoardScript : MonoBehaviour
+{
+    //public Transform cam;
+    private Transform player;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.LookAt(transform.position + player.forward);
+    }
+}
